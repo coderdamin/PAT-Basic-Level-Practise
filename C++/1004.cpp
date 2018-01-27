489e5ddd5c307a50642606e1e4121b60ba3df7be
coderdamin <amin_z@126.com>
2018-01-28 00:48:03 +0800

    Practise 4/5

--- C++/1004.cpp
+++ C++/1004.cpp
@@ -21,9 +21,49 @@
 //Joe Math990112
 
 #include <iostream>
+#include <memory.h>
+#include <stdlib.h>
 
 using namespace std;
+class Student {
+public:
+	Student() {
+		nScore = 0;
+		memset(acName, 0, sizeof(acName));
+		memset(acStudentid, 0, sizeof(acStudentid));
+	}
+	~Student() {}
+
+public:
+	int nScore;
+	char acName[11];
+	char acStudentid[11];
+};
 
 int main() {
-
+	int nCount = 0;
+	cin >> nCount;
+	int nScore = 0;
+	char acName[11] = { 0 };
+	char acStudentid[11] = { 0 };
+	Student scoreMax;
+	Student scoreMin;
+	scoreMax.nScore = -1;
+	scoreMin.nScore = 101;
+	for (int i = 0; i < nCount; ++i) {
+		cin >> acName >> acStudentid >> nScore;
+		if (nScore > scoreMax.nScore) {
+			scoreMax.nScore = nScore;
+			strcpy(scoreMax.acName, acName);
+			strcpy(scoreMax.acStudentid, acStudentid);
+		}
+		if (nScore < scoreMin.nScore) {
+			scoreMin.nScore = nScore;
+			strcpy(scoreMin.acName, acName);
+			strcpy(scoreMin.acStudentid, acStudentid);
+		}
+	}
+	cout << scoreMax.acName << ' ' << scoreMax.acStudentid << '\n';
+	cout << scoreMin.acName << ' ' << scoreMin.acStudentid << endl;
+	return 0;
 }
